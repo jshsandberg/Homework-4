775843578b1d2628082fa773b5363246ff49e12b
Joshua Sandberg <jshsandberg@gmail.com>
2020-07-08 18:20:29 -0700

    cleaner JS file

---
+++ quiz2.js
@@ -0,0 +1,90 @@
+//start quiz button click event listener;
+let timeFun;
+
+document.getElementById("start").addEventListener("click", ()=>{
+    timeFun = setInterval(timer, 1000);
+    appendQ(); 
+    });
+const qDiv = document.getElementById("qDiv");
+const aDiv = document.getElementById("aDiv");
+const msg = document.getElementById("msg")
+let current = 0;
+let time = 60;
+let answer = 0;
+const qArray= [
+    {
+        q: "What is my LEAST favorite topping for pizza?",
+        a: ["Pineapple", "Tire", "Pepperoni", "Olives"],
+        c: "Tire"
+    },
+    {
+        q: "Who is my least favorite brother?",
+        a: ["Jacob Sandberg", "Jacob Sandberg", "Jacob Sandberg", "Jacob Sandberg"],
+        c: "Jacob Sandberg"
+    },
+    {
+        q: "Whats the difference between a moodle and padoodle?",
+        a: ["Skadoodle", "Nothing", "One starts with M and the other a P", "One is a verb and the other is a noun"],
+        c: "Skadoodle"
+    },
+    {
+        q: "How awesome is this quiz from a scale of 1 to 5?",
+        a: ["5", "Perfect", "10", "Josh is an amazing person and human being"],
+        c: "Josh is an amazing person and human being"
+    },
+    {
+        q: "What is green and round?",
+        a: ["Circular Grass", "Green Wheel", "The Sun", "A circular picture of slime"],
+        c: "Green Wheel"
+    },
+    {
+        q: "Great Work! Let's see how you did!",
+        a: "",
+        c: ""
+    }
+]
+
+aDiv.addEventListener("click", function(event){
+    checkAnswer(event.target.textContent)
+})
+
+
+
+function appendQ(){
+    aDiv.innerHTML = ""
+    qDiv.textContent = qArray[current].q;
+    for(let i = 0;i<qArray[current].a.length; i++){
+       const btn = document.createElement("button");
+       btn.textContent = qArray[current].a[i];
+       aDiv.appendChild(btn);
+       btn.setAttribute(`class`, "button")
+    }
+}
+
+function checkAnswer(str){
+    if(qArray[current].c === str){
+        answer += 1;
+        msg.textContent = "CORRECT!";
+        
+    }else{
+        msg.textContent = "WRONG!";
+        time - 5000
+    }
+    current ++;
+    appendQ();
+    setTimeout(()=>msg.textContent = "", 1500)
+}
+
+function timer(){
+    document.getElementById("time").textContent = time;
+    time--;
+    if(time === 0){
+        aDiv.textContent = "All Done! Lets see how you did!";
+       clearInterval(timeFun);
+    }
+}
+
+console.log(answer)
+
+
+
